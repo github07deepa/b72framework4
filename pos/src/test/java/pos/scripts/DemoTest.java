2b5c3b9db959e61db8f9ce676b6a7cd4d8508100
vinod <vinod@host.docker.internal>
2023-06-26 12:55:58 +0530

    POS Framework added

---
+++ pos/src/test/java/pos/scripts/DemoTest.java
@@ -0,0 +1,15 @@
+package pos.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class DemoTest extends BaseTest {
+
+	@Test
+	public void testA()
+	{
+		Reporter.log("TestA", true);
+	}
+}
